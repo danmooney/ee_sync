375cb97937f9e7dad118b30fac5a22de44d8a29d
danmooney <dan.r.mooney@gmail.com>
2015-06-07 21:09:26 -0400

    Add Syncee_Response class

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Response.php
@@ -0,0 +1,107 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Response
+{
+    /**
+     * @var string
+     */
+    private $_raw_response;
+
+    private $_response_decoded;
+
+    /**
+     * @var int
+     */
+    private $_status_code;
+
+    /**
+     * @var Syncee_Site
+     */
+    private $_site;
+
+    /**
+     * Execute the request and store/parse the response
+     * @param Syncee_Request $request
+     * @param Syncee_Site $site
+     * @throws Syncee_Exception
+     */
+    public function __construct(Syncee_Request $request, Syncee_Site $site)
+    {
+        $this->_site         = $site;
+
+        $curl_handle         = $request->getCurlHandle();
+        $this->_raw_response = $response = $request->execute();
+        $this->_status_code  = (int) $curl_handle->http_status_code;
+
+        $decoded_response   = json_decode($response, true);
+
+        if (is_array($decoded_response) && isset($decoded_response['data']) && is_string($decoded_response['data'])) {
+            $this->_decryptResponseData($site, $decoded_response['data']);
+
+            $decoded_response['data']  = $this->_decryptResponseData($site, $decoded_response['data']);
+            $this->_response_decoded   = $decoded_response;
+            $this->_raw_response       = json_encode($decoded_response);
+        }
+    }
+
+    public function getStatusCode()
+    {
+        return $this->_status_code;
+    }
+
+    public function getRawResponse()
+    {
+        return $this->_raw_response;
+    }
+
+    public function getResponseDecoded()
+    {
+        return $this->_response_decoded;
+    }
+
+    public function getResponseDataDecoded()
+    {
+        return is_array($this->_response_decoded) && isset($this->_response_decoded['data'])
+            ? json_decode($this->_response_decoded['data'], true)
+            : false
+        ;
+    }
+
+    /**
+     * @return Syncee_Collection_Abstract
+     */
+    public function getResponseDataDecodedAsCollection()
+    {
+        // TODO
+    }
+
+    public function __toString()
+    {
+        return $this->_raw_response;
+    }
+
+    private function _decryptResponseData(Syncee_Site $site, $data)
+    {
+        $crypt = $site->rsa->getCrypt();
+        $crypt->loadKey($site->rsa->getPrivateKey());
+        return $crypt->decrypt(base64_decode($data));
+    }
+}
