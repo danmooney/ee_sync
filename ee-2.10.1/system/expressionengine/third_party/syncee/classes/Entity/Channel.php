79e65191bc32f0089b109f01967507990c371e9f
danmooney <dan.r.mooney@gmail.com>
2015-06-13 13:21:05 -0400

    Add instantiation of Syncee_Entity_Channel_Field_Collection for fields inside Syncee_Entity_Channel::__construct

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Channel.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Channel.php
@@ -20,5 +20,27 @@ if (!defined('SYNCEE_PATH')) {
 
 class Syncee_Entity_Channel extends Syncee_Entity_Abstract
 {
+    /**
+     * @var Syncee_Entity_Channel_Field_Collection
+     */
+    private $_fields;
 
+    protected $_ignored_columns_in_comparison = array(
+        'channel_url'
+    );
+
+    public function __construct(array $row = array(), $is_new = true) // TODO - is entity going to really extend active record.  It sorta makes sense.  But channel logic is way more complicated and is based in multiple tables.
+    {
+        if (isset($row['fields'])) {
+            $this->_fields = new Syncee_Entity_Channel_Field_Collection($row['fields']);
+            unset($row['fields']);
+        }
+
+        parent::__construct($row, $is_new);
+    }
+
+    public function getFields()
+    {
+        return $this->_fields;
+    }
 }
