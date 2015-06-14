30a939111c6b64bb281b91b69981b8981e6fa63e
danmooney <dan.r.mooney@gmail.com>
2015-06-14 15:53:19 -0400

    Add Syncee_Site_Collection::getChannelFieldComparisonCollectionLibrary

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Collection.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Collection.php
@@ -81,5 +81,38 @@ class Syncee_Site_Collection extends Syncee_Collection_Abstract
         return $site_channel_comparison_library;
     }
 
+    public function getChannelFieldComparisonCollectionLibrary()
+    {
+        $site_channel_field_library    = new Syncee_Entity_Channel_Field_Collection_Library();
+
+        // get channels/fields first
+        $channel_remote_request_entity = new Syncee_Request_Remote_Entity_Channel();
+
+        /**
+         * @var $row Syncee_Site
+         * @var $channel_collection Syncee_Entity_Channel_Collection
+         * @var $channel_entity Syncee_Entity_Channel
+         */
+        foreach ($this->_rows as $row) {
+            $request            = new Syncee_Request();
+            $response           = $request->makeEntityCallToSite($row, $channel_remote_request_entity);
+            $channel_collection = $response->getResponseDataDecodedAsCollection();
+
+            // get fields from channel collection and add to channel field collection
+            foreach ($channel_collection as $channel_entity) {
+                $channel_field_collection = $channel_entity->getFieldCollection();
+                $channel_field_collection->setSite($row);
+
+                if (!$site_channel_field_library->collectionAlreadyExistsInLibrary($channel_field_collection)) {
+                    $site_channel_field_library->appendToLibraryAsCollection($channel_field_collection);
+                }
+            }
+        }
+
+        $site_channel_field_comparison_library = $site_channel_field_library->compareCollections();
+
+        return $site_channel_field_comparison_library;
+    }
+
     // TODO - implement getSynchronizationProfileCollection
 }
