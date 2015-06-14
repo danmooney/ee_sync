e7e4aa785a9f990a2a711d2e736460cf99c8e781
danmooney <dan.r.mooney@gmail.com>
2015-06-14 19:39:06 -0400

    Add testTwoSitesWithDifferingFieldLabelGivesOneResultInComparisonLibrary

--- ee-2.10.1/system/expressionengine/third_party/syncee/tests/test.comparison_channel_field.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/tests/test.comparison_channel_field.php
@@ -24,7 +24,13 @@ class Test_Comparison_Channel_Field extends Syncee_Unit_Test_Case_Abstract
     protected $_truncation_setup_type = self::TRUNCATE_ALL;
 
     protected $_seed_data_files = array(
-        'animals_channel',
+        'testTwoSitesWithCompletelySimilarChannelFieldsGivesEmptyComparisonLibrary' => array(
+            'animals_channel',
+        ),
+        'testTwoSitesWithDifferingFieldLabelGivesOneResultInComparisonLibrary' => array(
+            'animals_channel',
+            'synchronization_tests/animals_channel_field_alter_field_label_in_source'
+        ),
     );
 
     public function setUp()
@@ -45,5 +51,27 @@ class Test_Comparison_Channel_Field extends Syncee_Unit_Test_Case_Abstract
     {
         $channel_field_comparison_library = $this->_site_collection->getChannelFieldComparisonCollectionLibrary();
         $this->assertTrue($channel_field_comparison_library->hasNoComparisons(), 'Two sites have no comparisons; they are the same');
+    }
+
+    public function testTwoSitesWithDifferingFieldLabelGivesOneResultInComparisonLibrary()
+    {
+        $this->fail('Need to implement ' . __METHOD__);
+        $channel_field_comparison_library = $this->_site_collection->getChannelFieldComparisonCollectionLibrary();
+
+        $non_empty_channel_field_comparison_library = $channel_field_comparison_library->getNonEmptyComparisonCollectionLibrary();
+
+        $this->assertEqual(count($non_empty_channel_field_comparison_library), 1, 'Number of non-empty channel field comparison collections is 1');
+
+        $channel_field_comparison_collection        = $non_empty_channel_field_comparison_library[0];
+        $this->assertEqual(count($channel_field_comparison_collection), 1, 'There is 1 comparison result entity in channel field comparison collection');
+
+        /**
+         * @var $channel_field_comparison_entity Syncee_Entity_Comparison
+         */
+        $channel_field_comparison_entity = $channel_field_comparison_collection[0];
+        $this->assertEqual($channel_field_comparison_entity->getComparateColumnName(), 'field_label', 'Lone channel comparison entity\'s comparate column name is "field_label"');
+        $this->assertEqual($channel_field_comparison_entity->getSourceValue(), 'Animal Description YO', 'Lone channel comparison entity\'s source value is "Animal Description YO"');
+        $this->assertEqual($channel_field_comparison_entity->getTargetValue(), 'Animal Description', 'Lone channel comparison entity\'s target value is "Animal Description"');
+        $this->assertEqual($channel_field_comparison_entity->getComparisonResult(), $channel_field_comparison_entity::RESULT_COMPARATE_VALUE_DIFFERS, 'Lone channel field comparison entity is ' . $channel_field_comparison_entity::RESULT_COMPARATE_VALUE_DIFFERS);
     }
 }
