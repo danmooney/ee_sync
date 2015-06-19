9f239d373bd345bd61d9c12446fb444bbb494f87
danmooney <dan.r.mooney@gmail.com>
2015-06-18 23:01:28 -0400

    Add Syncee_Entity_Comparate_Collection_Abstract

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Comparate/Collection/Abstract.php
@@ -0,0 +1,45 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+abstract class Syncee_Entity_Comparate_Collection_Abstract extends Syncee_Collection_Abstract
+    implements Syncee_Site_Storage_Interface
+{
+    /**
+     * @var Syncee_Site
+     */
+    protected $_site;
+
+    public function setSite(Syncee_Site $site)
+    {
+        $this->_site = $site;
+
+        /**
+         * @var $row Syncee_Entity_Abstract
+         */
+        foreach ($this->_rows as $row) {
+            $row->setSite($site);
+        }
+    }
+
+    public function getSite()
+    {
+        return $this->_site;
+    }
+}
