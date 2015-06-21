b23153311e7d46363a1e094506c917ba20564c52
danmooney <dan.r.mooney@gmail.com>
2015-06-20 23:54:48 -0400

    Add dynamic menu output

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/menu.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/menu.php
@@ -1,6 +1,30 @@
-<?= Syncee_Upd::MODULE_NAME . ' ' . Syncee_Upd::VERSION ?>
-<ul class="syncee-menu">
-    <li>Site Groups</li>
-    <li>Remote Sites</li>
-    <li>Help</li>
+<?php
+$menu = array(
+    'Site Groups' => array(
+        'method' => 'viewSiteGroupList'
+    ),
+    'Local Sites' => array(
+        'method' => 'viewLocalSiteList'
+    ),
+    'Remote Sites' => array(
+        'method' => 'viewRemoteSiteList'
+    ),
+    'Help' => array(
+        'method' => 'help'
+    ),
+);
+
+$current_method = ee()->input->get('method');
+
+?>
+<?= false; /*Syncee_Upd::MODULE_NAME . ' ' . Syncee_Upd::VERSION*/ ?>
+<ul class="menu">
+<?php
+    foreach ($menu as $label => $data): ?>
+    <li>
+        <a class="<?= $current_method === $data['method'] ? 'active' : 'not-active' ?>" href="<?= Syncee_Helper::createModuleCpUrl($data['method']) ?>"><?= $label ?></a>
+    </li>
+<?php
+    endforeach ?>
 </ul>
+<div class="clr"></div>
