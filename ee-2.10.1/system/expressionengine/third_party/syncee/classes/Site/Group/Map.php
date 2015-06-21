75310317bf17ad0d320699686a2b6d509deabc8b
danmooney <dan.r.mooney@gmail.com>
2015-06-20 23:53:14 -0400

    Add Syncee_Site_Group_Map class

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Group/Map.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Site_Group_Map extends Syncee_ActiveRecord_Abstract
+{
+    const TABLE_NAME = 'syncee_site_group_map';
+
+    protected $_primary_key_names = array('site_id', 'site_group_id');
+
+    protected $_belongs_to = array(
+        'Syncee_Site'        => 'site_id',
+        'Syncee_Site_Group'  => 'site_group_id'
+    );
+
+    protected static $_cols;
+}
