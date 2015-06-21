963c61c3b0d6002f4491e13534ca7a7216305902
danmooney <dan.r.mooney@gmail.com>
2015-06-21 00:48:33 -0400

    Add active menu item check based on class methods inside $mcp object

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/menu.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/menu.php
@@ -1,7 +1,7 @@
 <?php
 $menu = array(
     'Site Groups' => array(
-        'method' => 'viewSiteGroupList'
+        'method' => 'viewSiteGroupList',
     ),
     'Local Sites' => array(
         'method' => 'viewLocalSiteList'
@@ -17,12 +17,11 @@ $menu = array(
 $current_method = ee()->input->get('method');
 
 ?>
-<?= false; /*Syncee_Upd::MODULE_NAME . ' ' . Syncee_Upd::VERSION*/ ?>
 <ul class="menu">
 <?php
     foreach ($menu as $label => $data): ?>
     <li>
-        <a class="<?= $current_method === $data['method'] ? 'active' : 'not-active' ?>" href="<?= Syncee_Helper::createModuleCpUrl($data['method']) ?>"><?= $label ?></a>
+        <a class="<?= in_array($data['method'], get_class_methods(get_class($mcp))) ? 'active' : 'not-active' ?>" href="<?= Syncee_Helper::createModuleCpUrl($data['method']) ?>"><?= $label ?></a>
     </li>
 <?php
     endforeach ?>
