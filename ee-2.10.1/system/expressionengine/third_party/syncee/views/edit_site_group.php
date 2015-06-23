c1bd661485398e9bddf6cf3b15eab0405afafed8
danmooney <dan.r.mooney@gmail.com>
2015-06-22 22:13:58 -0400

    Change "Edit Site Group" to "Update Site Group"

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/edit_site_group.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/edit_site_group.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__) . '/../_init.php';
 <form method="post">
     <p>Site Group Name:</p>
     <input type="text" name="title" value="<?= $syncee_site_group->title ?>">
-    <button type="submit">Edit Site Group</button>
+    <button type="submit">Update Site Group</button>
     <input type="hidden" name="XID" value="<?= ee()->csrf->get_user_token() ?>">
     <input type="hidden" name="csrf_token" value="<?= ee()->csrf->get_user_token() ?>">
 </form>
