c994eb2595463d8f97e9adc69234aa9703c6d5b7
danmooney <dan.r.mooney@gmail.com>
2015-06-29 23:39:00 -0400

    Add Syncee_Mcp_Site_Remote_Request

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Mcp/Site/Remote/Request.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Mcp_Site_Remote_Request extends Syncee_Mcp_Abstract
+{
+    public function viewRemoteRequestLogList()
+    {
+        return Syncee_View::render(__FUNCTION__, array(
+
+        ), $this);
+    }
+}
