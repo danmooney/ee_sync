27360b7cf63c08c25badfe7a27ba491a2d3f6629
danmooney <dan.r.mooney@gmail.com>
2015-06-29 23:39:47 -0400

    Add Syncee_Site_Request_Log

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Request/Log.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Site_Request_Log extends Syncee_ActiveRecord_Abstract
+{
+    const TABLE_NAME = 'syncee_site_request_log';
+}
