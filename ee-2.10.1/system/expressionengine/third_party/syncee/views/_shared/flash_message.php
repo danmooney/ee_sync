59e32f9c991e9622ba7ade1173c15b7529b435ac
danmooney <dan.r.mooney@gmail.com>
2015-07-01 22:12:58 -0400

    Implement flash message template view

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/flash_message.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @var $flash_message string
+ * @var $flash_message_type string
+ */
+?>
+<div class="flash-message flash-message-type-<?= $flash_message_type ?>">
+    <?= $flash_message ?>
+</div>
