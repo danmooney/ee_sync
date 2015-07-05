639c34b46bb2101f8532d1247fd465928ed3abc1
danmooney <dan.r.mooney@gmail.com>
2015-07-05 12:54:21 -0400

    Add Syncee_Field_Error

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Field/Error.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Field_Error extends Syncee_Field
+{
+    const FIELD_ERROR_REQUIRED_BUT_EMPTY    = 'FIELD_ERROR_REQUIRED_BUT_EMPTY';
+    const FIELD_ERROR_OPTION_DOES_NOT_EXIST = 'FIELD_ERROR_OPTION_DOES_NOT_EXIST';
+}
