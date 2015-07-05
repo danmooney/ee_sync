64f818bf7e7c2ab198b2bca0563076de8f4e7f85
danmooney <dan.r.mooney@gmail.com>
2015-07-05 14:25:15 -0400

    Wrap remote site title in <strong>

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/delete_remote_site.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/delete_remote_site.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__) . '/../_init.php';
 ?>
 <h1><?= "{$syncee_remote_site->getSiteUrl()}" ?></h1>
 <h2>(EE Site ID: <?= $syncee_remote_site->ee_site_id ?>)</h2><br>
-<p>Are you sure you'd like to delete <?= $syncee_remote_site->title ?: 'this remote site' ?>?</p>
+<p>Are you sure you'd like to delete <strong><?= $syncee_remote_site->title ?></strong>?</p>
 
 <form method="post">
     <button class="btn" type="submit">Delete Remote Site</button>
