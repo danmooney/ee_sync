63c83f6bc942b13dd7c7e7a5eede84dbe29baf55
danmooney <dan.r.mooney@gmail.com>
2015-07-04 21:12:34 -0400

    Change edit_site_group template to simply invoke Syncee_Form_Site_Group::__toString

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/edit_site_group.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/edit_site_group.php
@@ -2,26 +2,8 @@
 /**
  * @var $ee_sites array
  * @var $syncee_site_group Syncee_Site_Group
+ * @var $form Syncee_Form_Abstract
  */
 require_once dirname(__FILE__) . '/../_init.php';
 
-?>
-
-<form method="post">
-    <label for="title">Enter your Site Group Name</label><br>
-    <input id="title" type="text" name="title" value="<?= $syncee_site_group->title ?>"><br><br>
-    <label for="ee_site_id">Choose a Local Site</label><br>
-    <select id="ee_site_id" name="ee_site_id">
-        <option value="" disabled selected>Select a Local Site</option>
-    <?php
-        foreach ($ee_sites as $ee_site):
-            $local_syncee_site = $syncee_site_group->getSiteCollection()->filterByCondition(array('is_local' => true), true); ?>
-            <option <?= $local_syncee_site->getPrimaryKeyValues(true) === $ee_site->site_id ? 'selected="selected"' : '' ?> value="<?= $ee_site->site_id ?>"><?= $ee_site->site_label ?></option>
-    <?php
-        endforeach ?>
-    </select>
-    <br><br><br>
-    <button class="btn" type="submit"><?= $syncee_site_group->isEmptyRow() ? 'Add New' : 'Update' ?> Site Group</button>
-    <input type="hidden" name="XID" value="<?= ee()->csrf->get_user_token() ?>">
-    <input type="hidden" name="csrf_token" value="<?= ee()->csrf->get_user_token() ?>">
-</form>
+echo $form;
