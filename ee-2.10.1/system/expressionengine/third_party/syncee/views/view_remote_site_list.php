4357e593a2d1cc4289bc0eb8baf0da28d13e6a08
danmooney <dan.r.mooney@gmail.com>
2015-07-09 22:03:47 -0400

    Center some columns

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/view_remote_site_list.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/view_remote_site_list.php
@@ -21,8 +21,7 @@ require_once dirname(__FILE__) . '/../_init.php';
                     <th>URL</th>
                     <th>EE Site ID</th>
                     <th>EE Action ID</th>
-                    <?php /* <th>Call Over HTTPS?</th> */ ?>
-                    <?php /* <th>IP Whitelist</th> */ ?>
+                    <th>Call Over HTTPS?</th>
                     <th>Date Created</th>
                     <th>Syncee Site ID</th>
                     <th>Edit</th>
@@ -40,9 +39,9 @@ require_once dirname(__FILE__) . '/../_init.php';
                 <td align="center"><a href="<?= $syncee_remote_site->getSiteUrl() ?>" target="_blank"><?= $syncee_remote_site->getSiteUrl() ?></a></td>
                 <td align="right"><?= $syncee_remote_site->ee_site_id ?></td>
                 <td align="right"><?= $syncee_remote_site->action_id ?></td>
-                <?php /*<td><?= $syncee_remote_site->use_https ? 'Yes' : 'No' ?></td> */ ?>
+                <td align="center"><?= $syncee_remote_site->use_https ? 'Yes' : 'No' ?></td>
                 <?php /* <td><?= $syncee_remote_site->ip_whitelist ?></td> */ ?>
-                <td><?= Syncee_Helper::convertUTCDateToLocalizedHumanDatetime($syncee_remote_site->create_datetime) ?></td>
+                <td align="center"><?= Syncee_Helper::convertUTCDateToLocalizedHumanDatetime($syncee_remote_site->create_datetime) ?></td>
                 <td align="right"><?= $syncee_remote_site->getPrimaryKeyValues(true) ?></td>
                 <td align="center"><a href="<?= Syncee_Helper::createModuleCpUrl('editRemoteSite', $primary_key_value_map) ?>">Edit</a></td>
                 <td align="center"><a href="<?= Syncee_Helper::createModuleCpUrl('deleteRemoteSite', $primary_key_value_map) ?>">Delete</a></td>
