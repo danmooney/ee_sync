b2e8d6ff7f23ad2323836064159fc1b1486d0c80
danmooney <dan.r.mooney@gmail.com>
2015-07-12 20:53:01 -0400

    Implement Syncee_Paginator

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Paginator.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Paginator.php
@@ -28,8 +28,98 @@ class Syncee_Paginator
 
     protected $_offset;
 
+    protected $_total_rows;
+
     public function __construct(array $params)
     {
+        foreach ($params as $key => $val) {
+            $protected_member_name = '_' . $key;
+            if (property_exists($this, $protected_member_name)) {
+                $this->$protected_member_name = $val;
+            }
+        }
+    }
 
+    /**
+     * Modify query based on pagination parameters passed through GET
+     * @param $db
+     */
+    public function modifyQueryOnDriver($db)
+    {
+        $count_db          = clone $db;
+        $this->_total_rows = $total_rows = $count_db->get()->num_rows();
+
+        if ($this->_order_by) {
+            $db->order_by($this->_order_by, $this->_order_dir);
+        }
+
+        if ($this->_count_per_page) {
+            $db->limit($this->_count_per_page);
+        }
+
+        if ($this->_offset && $this->_offsetIsLessThanTotalRows()) {
+            $db->offset($this->_offset);
+        }
+    }
+
+    public function getCurrentPageNumber()
+    {
+        return (int) ceil($this->_offset / $this->_count_per_page) + 1;
+    }
+
+    public function hasNextPage()
+    {
+        return $this->_offset + $this->_count_per_page < $this->_total_rows;
+    }
+
+    public function hasPrevPage()
+    {
+        return $this->_offset - $this->_count_per_page >= 0;
+    }
+
+//    public function getPrevOffset($page_number = null)
+//    {
+//        if (!$page_number && !$this->hasPrevPage()) {
+//            return false;
+//        }
+//
+//        if (!$page_number) {
+//            $page_number = $this->getCurrentPageNumber();
+//        }
+//
+//        return ($this->_count_per_page * $page_number) - $this->_count_per_page;
+//    }
+
+    public function getCurrentOffset()
+    {
+        return $this->_offset;
+    }
+
+    public function getOffsetByPageNumber($page_number = null)
+    {
+        return ($this->_count_per_page * ($page_number - 1));
+    }
+
+//    public function getNextOffset($page_number = null)
+//    {
+//        if (!$page_number && !$this->hasNextPage()) {
+//            return false;
+//        }
+//
+//        if (!$page_number) {
+//            $page_number = $this->getCurrentPageNumber();
+//        }
+//
+//        return ($this->_count_per_page * $page_number) + $this->_count_per_page;
+//    }
+
+    public function getTotalPages()
+    {
+        return ceil($this->_total_rows / $this->_count_per_page);
+    }
+
+    private function _offsetIsLessThanTotalRows()
+    {
+        return $this->_offset < $this->_total_rows;
     }
 }
