181a440a42b25b6d7c3b22621a1acb3d0e7d0cd6
danmooney <dan.r.mooney@gmail.com>
2015-07-14 21:50:59 -0400

    Add diagnosis evaluation to Syncee_Site_Request_Log_Diagnosis

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Request/Log/Diagnosis.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Request/Log/Diagnosis.php
@@ -21,22 +21,63 @@ if (!defined('SYNCEE_PATH')) {
 /**
  * Class Syncee_Site_Request_Log_Diagnosis
  */
-class Syncee_Site_Request_Log_Diagnosis extends Syncee_Entity_Abstract
+class Syncee_Site_Request_Log_Diagnosis
 {
+    const REQUEST_DIAGNOSIS_SYNCEE_VERSIONS_DIFFER = 'REQUEST_DIAGNOSIS_SYNCEE_VERSIONS_DIFFER';
+    const REQUEST_DIAGNOSIS_UNABLE_TO_REACH_SERVER = 'REQUEST_DIAGNOSIS_UNABLE_TO_REACH_SERVER';
+    const REQUEST_DIAGNOSIS_ACTION_ID_INVALID      = 'REQUEST_DIAGNOSIS_ACTION_ID_INVALID';
+    const REQUEST_FORBIDDEN_FROM_THIS_IP           = 'REQUEST_FORBIDDEN_FROM_THIS_IP';
+    const REQUEST_FORBIDDEN_FROM_MASTER_OVERRIDE   = 'REQUEST_FORBIDDEN_FROM_MASTER_OVERRIDE';
+
     /**
      * @var Syncee_Site_Request_Log
      */
     private $_request_log;
 
+    private $_diagnoses = array();
 
     public function __construct(Syncee_Site_Request_Log $request_log)
     {
         $this->_request_log = $request_log;
+
+        if ($request_log->isEmptyRow()) {
+            return;
+        }
+
         $this->_diagnose();
+    }
+
+    public function getDiagnoses()
+    {
+        return $this->_diagnoses;
     }
 
     private function _diagnose()
     {
+        $request_log = $this->_request_log;
+        $diagnoses   = array();
+        $code        = intval($request_log->code);
 
+        if ($code === 0) {
+            $diagnoses[] = static::REQUEST_DIAGNOSIS_UNABLE_TO_REACH_SERVER;
+        }
+
+        if ($code === 403) {
+            if (stripos($request_log->message, 'master override') !== false) {
+                $diagnoses[] = static::REQUEST_FORBIDDEN_FROM_MASTER_OVERRIDE;
+            } else {
+                $diagnoses[] = static::REQUEST_FORBIDDEN_FROM_THIS_IP;
+            }
+        }
+
+        if ($request_log->content_type === 'text/html') {
+            $diagnoses[] = static::REQUEST_DIAGNOSIS_ACTION_ID_INVALID;
+        }
+
+        if ($request_log->version && $request_log->version !== Syncee_Upd::VERSION) {
+            $diagnoses[] = static::REQUEST_DIAGNOSIS_SYNCEE_VERSIONS_DIFFER;
+        }
+
+        $this->_diagnoses = array_unique($diagnoses);
     }
 }
