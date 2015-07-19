eb23b6b8d1406109eb41b8b54e08596d1571f121
danmooney <dan.r.mooney@gmail.com>
2015-07-18 22:18:31 -0400

    Incorporate formatters

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Table.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Table.php
@@ -30,10 +30,27 @@ class Syncee_Table
      */
     private $_row_collection;
 
-    public function __construct(Syncee_Table_Column_Collection $column_collection, Syncee_Collection_Abstract $row_collection)
+    /**
+     * @var Syncee_Table_Row_Formatter_Abstract
+     */
+    private $_table_row_formatter;
+
+    /**
+     * @var Syncee_Table_Formatter_Abstract
+     */
+    private $_table_formatter;
+
+
+    public function __construct(
+        Syncee_Table_Column_Collection $column_collection,
+        Syncee_Collection_Abstract $row_collection,
+        Syncee_Table_Row_Formatter_Abstract $table_row_formatter = null,
+        Syncee_Table_Formatter_Abstract $table_formatter_collection = null)
     {
-        $this->_column_collection = $column_collection;
-        $this->_row_collection    = $row_collection;
+        $this->_column_collection              = $column_collection;
+        $this->_row_collection                 = $row_collection;
+        $this->_table_row_formatter = $table_row_formatter;
+        $this->_table_formatter     = $table_formatter_collection;
     }
 
     public function __toString()
@@ -59,25 +76,42 @@ class Syncee_Table
          * @var $formatter Syncee_Table_Column_Value_Formatter_Abstract
          */
         foreach ($this->_row_collection as $row) {
-            $html .= '<tr>';
+            $html .= sprintf(
+                '<tr%s>',
+                $this->_table_row_formatter
+                    ? ' ' . $this->_table_row_formatter->formatBasedOnRow($row)
+                    : ''
+            );
 
             foreach ($this->_column_collection as $column) {
                 $html .= sprintf('<td align="%s">', $column->getAlign());
                 // TODO - put __toString in $column???
-                if (is_callable($column->getColumnReferenceValue())) {
+                if (is_callable($column->getColumnReferenceValue()) && !is_scalar($column->getColumnReferenceValue())) { // is_callable will return true for global functions; need to check if the reference value isn't scalar in addition to alleviate that issue
                     $callable         = $column->getColumnReferenceValue();
                     $column_contents  = $callable($row);
                 } elseif (null === $column->getColumnReferenceValue()) {
                     $column_contents = $column->getLabel();
-                } else {
+                } elseif ($row->hasColumn($column->getColumnReferenceValue())) {
                     $column_contents = $row->{$column->getColumnReferenceValue()};
+                } else {
+                    $column_contents = $column->getColumnReferenceValue();
                 }
 
                 if ($formatter = $column->getFormatter()) {
                     $column_contents = $formatter->format($column_contents, $row);
                 }
 
-                if (strip_tags($column_contents) === $column_contents) {
+                $column_contents                      = trim($column_contents);
+                $column_contents_have_no_html_wrapper = (
+                    substr($column_contents, 0, 1) !== '<' ||
+                    substr($column_contents, -1) !== '>'
+                );
+
+                if ($column_contents_have_no_html_wrapper) {
+                    if (!strlen($column_contents)) {
+                        $column_contents = '<i>(N/A)</i>';
+                    }
+
                     $column_contents = sprintf('<span>%s</span>', $column_contents);
                 }
 
