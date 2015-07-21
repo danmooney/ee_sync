32ec0ccfd345f905616701f4099bc2e065e07197
danmooney <dan.r.mooney@gmail.com>
2015-07-21 19:53:53 -0400

    Sort collections in channel comparison library by source site primary key values

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Mcp/Site/Group.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Mcp/Site/Group.php
@@ -51,7 +51,14 @@ class Syncee_Mcp_Site_Group extends Syncee_Mcp_Abstract
         $site_group_id     = ee()->input->get('site_group_id');
         $syncee_site_group = Syncee_Site_Group::findByPk($site_group_id);
 
-        $channel_comparison_library = $syncee_site_group->getSiteCollection()->getChannelComparisonCollectionLibrary();
+        $channel_comparison_library = $syncee_site_group->getSiteCollection()->getChannelComparisonCollectionLibrary()/*->getNonEmptyComparisonCollectionLibrary()*/;
+
+        // sort collections alphabetically by source site primary key.
+        // this is to have a known and predictable way to iterate over collections and get everything in the right order.
+        $channel_comparison_library->sortByCallback(function (Syncee_Entity_Comparison_Collection $a, Syncee_Entity_Comparison_Collection $b) {
+            return $b->getSource()->getSite()->getPrimaryKeyValues(true) - $a->getSource()->getSite()->getPrimaryKeyValues(true);
+        });
+
         $request_log_collection     = $syncee_site_group->getSiteCollection()->getRequestLogCollection();
 
         return Syncee_View::render(__FUNCTION__, array(
