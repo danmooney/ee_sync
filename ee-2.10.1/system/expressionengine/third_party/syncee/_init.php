a9b144f119ce1a078a2da02e69bf24661bf2b3bb
danmooney <dan.r.mooney@gmail.com>
2015-07-28 21:01:28 -0400

    Add more constants and display PHP requirements to end user when their system fails to meet them

--- ee-2.10.1/system/expressionengine/third_party/syncee/_init.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/_init.php
@@ -12,20 +12,51 @@ if (!extension_loaded('json')) {
     die('JSON extension required for this module.');
 }
 
+if (!extension_loaded('curl')) {
+    header('HTTP 1.1 500 Internal Server Error', true, 500);
+    die('Curl extension required for this module.');
+}
+
 // JSON_PRETTY_PRINT is PHP 5.4+
 if (!defined('JSON_PRETTY_PRINT')) {
     define('JSON_PRETTY_PRINT', 0);
 }
 
-$minimum_php_version = '5.3';
+$minimum_php_version_major_and_minor_only = '5.3';
+$php_version_major_and_minor_only         = preg_replace('#\.\d+$#', '', PHP_VERSION);
 
-if (version_compare(PHP_VERSION, $minimum_php_version) < 0) {
+
+if (version_compare($php_version_major_and_minor_only, $minimum_php_version_major_and_minor_only) < 0) {
     header('HTTP 1.1 500 Internal Server Error', true, 500);
-    die("PHP version must be at least $minimum_php_version;  the version installed on the server is " . PHP_VERSION . '.  Please upgrade in order to use this module.');
+    die("PHP version must be at least $minimum_php_version_major_and_minor_only;  the version installed on the server is " . $php_version_major_and_minor_only . '.  Please upgrade in order to use this module.');
+}
+
+// This addon just isn't gonna work with all that eval'd code EE likes to do if short_open_tag is off.
+// '<?=' is the only short tag in use in this addon so PHP 5.4+ will not have any issue as long as we're not eval'ing the code.
+// The reason it can't be eval'd is because magic constant __FILE__ will simply not produce the proper value in an eval'd context.
+$php_version_is_53_or_less                         = version_compare($php_version_major_and_minor_only, '5.3') <= 0;
+$php_unable_to_parse_short_open_tag                = $php_version_is_53_or_less && !ini_get('short_open_tag');
+$ee_will_eval_templates                            = !ini_get('short_open_tag') && config_item('rewrite_short_tags');
+$ee_will_literally_output_unparsed_short_open_tags = $php_unable_to_parse_short_open_tag && !config_item('rewrite_short_tags');
+
+if ($php_unable_to_parse_short_open_tag) {
+    header('HTTP 1.1 500 Internal Server Error', true, 500);
+    die('short_open_tag PHP setting must be turned on, or you must upgrade to PHP 5.4 or later.');
+}
+
+if ($ee_will_literally_output_unparsed_short_open_tags) {
+    header('HTTP 1.1 500 Internal Server Error', true, 500);
+    die('rewrite_short_tags EE config item must be turned on in order for this addon to work on your installation.');
+}
+
+if ($ee_will_eval_templates) {
+    header('HTTP 1.1 500 Internal Server Error', true, 500);
+    die('short_open_tag PHP setting must be turned on, or you must turn off the rewrite_short_tags EE config item.');
 }
 
 // TODO - start tracking memory usage in tests/for the entire module in general while building
-
+defined('SYNCEE_VERSION')         or define('SYNCEE_VERSION',        '0.1.0');
+defined('SYNCEE_URL')             or define('SYNCEE_URL',            'http://www.sync-ee.com');
 defined('SYNCEE_PATH')            or define('SYNCEE_PATH',           dirname(__FILE__));
 defined('SYNCEE_PATH_TESTS')      or define('SYNCEE_PATH_TESTS',     SYNCEE_PATH . '/tests');
 defined('SYNCEE_TEST_MODE')       or define('SYNCEE_TEST_MODE',      isset($_SERVER['SYNCEE_TEST_MODE']) && $_SERVER['SYNCEE_TEST_MODE']);
