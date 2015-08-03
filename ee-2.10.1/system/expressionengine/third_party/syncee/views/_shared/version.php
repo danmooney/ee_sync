f6d875cf4cea88cb27fcfdf93e0f4a449c5e058a
danmooney <dan.r.mooney@gmail.com>
2015-08-02 21:02:13 -0400

    Change SYNCEE_VERSION constant reference

--- ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/version.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/views/_shared/version.php
@@ -1,1 +1,1 @@
-<span class="syncee-version"><?= Syncee_Upd::MODULE_NAME . ' ' . Syncee_Upd::VERSION ?><a class="btn go-pro" href="#">Go Pro</a></span>
+<span class="syncee-version"><?= Syncee_Upd::MODULE_NAME . ' ' . SYNCEE_VERSION ?><a class="btn go-pro" href="#">Go Pro</a></span>
