0a4b70e73c2ff16d97d07d7e7ec766d70f3e211f
danmooney <dan.r.mooney@gmail.com>
2015-08-07 22:06:16 -0400

    Add EE3 setup file

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/addon.setup.php
@@ -0,0 +1,20 @@
+<?php
+
+// EE3 setup file
+
+if (!function_exists('lang')) {
+    function lang($line, $id = '')
+    {
+        $CI = get_instance();
+        return $CI->lang->line($line, $id);
+    }
+}
+
+return array(
+    'author' => 'Dan Mooney',
+    'author_url' => 'https://sync-ee.com/',
+    'name' => lang('syncee_module_name'),
+    'description' => lang('syncee_module_description'),
+    'version' => '0.1.0',
+    'settings_exist' => true,
+);
