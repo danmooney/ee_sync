4ff97a1b35a95bd003914701e5fdfb8f254b489c
danmooney <dan.r.mooney@gmail.com>
2015-08-14 15:44:34 -0400

    Add merge arrow display calculations/logic

--- ee-2.10.1/themes/third_party/syncee/js/comparison.js
+++ ee-2.10.1/themes/third_party/syncee/js/comparison.js
@@ -2,8 +2,79 @@ $(function ($) {
     var $comparisonCollectionTable = $('.comparison-collection-table'),
         summaryCheckboxesByColIdxAndSummaryRowIdx = [],
         resultCheckboxesByColIdxAndSummaryRowIdx = [],
-        resultCheckboxesByRowIdx = []
+        resultCheckboxesByRowIdx = [],
+        summaryRowIdxs = [],
+        colIdxCount = $comparisonCollectionTable.children('thead').find('tr th').length
     ;
+
+    $('[data-summary-row-idx]').each(function () {
+        summaryRowIdxs.push($(this).data('summary-row-idx'));
+    });
+
+    summaryRowIdxs = $.unique(summaryRowIdxs);
+
+    function getSummaryCheckboxesByColIdxAndSummaryRowIdx (colIdx, summaryRowIdx) {
+        if (!summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx]) {
+            summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx] = [];
+        }
+
+        if (!summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx]) {
+            summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx] = $comparisonCollectionTable.find('[data-row-idx="' + summaryRowIdx + '"]').find('[data-col-idx="' + colIdx + '"]').find(':checkbox');
+        }
+
+        return summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx];
+    }
+
+    function getResultCheckboxesByColIdxAndSummaryRowIdx (colIdx, summaryRowIdx) {
+        if (!resultCheckboxesByColIdxAndSummaryRowIdx[colIdx]) {
+            resultCheckboxesByColIdxAndSummaryRowIdx[colIdx] = [];
+        }
+
+        if (!resultCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx]) {
+            resultCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx] = $comparisonCollectionTable.find('[data-summary-row-idx="' + summaryRowIdx + '"]').find('[data-col-idx="' + colIdx + '"]').find(':checkbox');
+        }
+
+        return resultCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx];
+    }
+
+    // fetch all source checkboxes by summary row index
+    function getSourceCheckboxesBySummaryRowIdx (summaryRowIdx, arrOrigin) {
+        var $checkboxes = $();
+
+        // seed the data first
+        $.each(summaryRowIdxs, function (idx, summaryRowIdx) {
+            for (var colIdx = 1; colIdx < colIdxCount; colIdx += 1) {
+                getResultCheckboxesByColIdxAndSummaryRowIdx(colIdx, summaryRowIdx);
+                getSummaryCheckboxesByColIdxAndSummaryRowIdx(colIdx, summaryRowIdx);
+            }
+        });
+
+        if (!arrOrigin['sourcesBySummaryRowIdxOnly']) {
+            arrOrigin['sourcesBySummaryRowIdxOnly'] = [];
+        }
+
+        if (!arrOrigin['sourcesBySummaryRowIdxOnly'][summaryRowIdx]) {
+            $.each(arrOrigin, function (idx, arr) {
+                if (typeof idx === 'number' && arr && arr[summaryRowIdx]) {
+                    $checkboxes = $checkboxes.add(arr[summaryRowIdx].filter(function () {
+                        return $(this).closest('.source-field').length;
+                    }));
+                }
+            });
+
+            arrOrigin['sourcesBySummaryRowIdxOnly'][summaryRowIdx] = $checkboxes;
+        }
+
+        return arrOrigin['sourcesBySummaryRowIdxOnly'][summaryRowIdx];
+    }
+
+    function getResultCheckboxesByRowIdx (rowIdx) {
+        if (!resultCheckboxesByRowIdx[rowIdx]) {
+            resultCheckboxesByRowIdx[rowIdx] = $comparisonCollectionTable.find('[data-row-idx="' + rowIdx + '"]').find(':checkbox');
+        }
+
+        return resultCheckboxesByRowIdx[rowIdx];
+    }
 
     $comparisonCollectionTable.find('.comparison-summary').on('click', function (e) {
         if ($(e.target || e.srcElement).is(':input')) {
@@ -14,14 +85,63 @@ $(function ($) {
     });
 
     function calculateMergeResult ($checkbox) {
-        // TODO
+        var $cell = $checkbox.closest('td'),
+            $row  = $cell.closest('tr'),
+            colIdx = $cell.data('col-idx'),
+            rowIdx = $row.data('row-idx'),
+            summaryRowIdx = $row.data('summary-row-idx') || rowIdx,
+            $correspondingMergeCell = $cell.siblings('.merge-result'),
+            isTargetField = $cell.hasClass('target-field'),
+            isSourceField = !isTargetField,
+            isExistenceSummaryCell = $cell.hasClass('comparison-site-collection-existence-container'),
+            $summaryCheckbox = getSummaryCheckboxesByColIdxAndSummaryRowIdx(colIdx, summaryRowIdx),
+            $summaryMergeCell = $summaryCheckbox.closest('tr').find('.merge-result'),
+            checkedCheckboxesExistInTarget = (
+                getSummaryCheckboxesByColIdxAndSummaryRowIdx(1, summaryRowIdx).filter(':checked').length ||
+                getResultCheckboxesByColIdxAndSummaryRowIdx(1, summaryRowIdx).filter(':checked').length
+            ),
+            checkedCheckboxesExistInSources = (
+                getSourceCheckboxesBySummaryRowIdx(summaryRowIdx, summaryCheckboxesByColIdxAndSummaryRowIdx).filter(':checked').length ||
+                getSourceCheckboxesBySummaryRowIdx(summaryRowIdx, resultCheckboxesByColIdxAndSummaryRowIdx).filter(':checked').length
+            )
+        ;
+
+        if (!checkedCheckboxesExistInTarget) {
+            $correspondingMergeCell.removeClass('left');
+            $summaryMergeCell.removeClass('left');
+        } else {
+            $correspondingMergeCell.addClass('left');
+            $summaryMergeCell.addClass('left');
+        }
+
+        if (!checkedCheckboxesExistInSources) {
+            $correspondingMergeCell.removeClass('right');
+            $summaryMergeCell.removeClass('right');
+        } else {
+            $correspondingMergeCell.addClass('right');
+            $summaryMergeCell.addClass('right');
+        }
+
+        // TODO - update result text
+        if (isExistenceSummaryCell) {
+            //if (hasTargetCheckboxesChecked) {
+            //    $summaryMergeCell.addClass('left');
+            //}
+            //
+            //if (hasSourceCheckboxesChecked) {
+            //    $summaryMergeCell.addClass('right');
+            //}
+
+        } else {
+
+        }
     }
 
     function updateCheckbox (e, state, triggerOtherCheckboxesOnSummaryRow) {
         var isEvent = !!e.currentTarget,
             $checkbox = isEvent ? $(e.currentTarget) : e,
             isChecked = typeof state !== 'undefined' ? state : $checkbox.prop('checked'),
-            $cell = $checkbox.closest('td, th'),
+            $cell = $checkbox.closest('td'),
             $row = $checkbox.closest('tr'),
             isSummaryRow = !!$checkbox.closest('.comparison-summary').length,
             $comparisonSummaryRow,
@@ -55,29 +175,15 @@ $(function ($) {
             $cell.removeClass('clicked');
         }
 
-        calculateMergeResult($row);
-
         summaryRowIdx = $comparisonSummaryRow.data('row-idx');
 
-        if (!resultCheckboxesByColIdxAndSummaryRowIdx[colIdx]) {
-            resultCheckboxesByColIdxAndSummaryRowIdx[colIdx] = [];
-        }
+        getResultCheckboxesByColIdxAndSummaryRowIdx(colIdx, summaryRowIdx);
 
-        if (!resultCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx]) {
-            resultCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx] = $comparisonResultsRow.find('[data-col-idx="' + colIdx + '"]').find(':checkbox');
-        }
+        getResultCheckboxesByRowIdx(rowIdx);
 
-        if (!resultCheckboxesByRowIdx[rowIdx]) {
-            resultCheckboxesByRowIdx[rowIdx] = $comparisonResultsRow.find('[data-row-idx="' + rowIdx + '"]').find(':checkbox');
-        }
+        getSummaryCheckboxesByColIdxAndSummaryRowIdx(colIdx, summaryRowIdx);
 
-        if (!summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx]) {
-            summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx] = [];
-        }
-
-        if (!summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx]) {
-            summaryCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx] = $comparisonSummaryRow.find('[data-col-idx="' + colIdx + '"]').find(':checkbox');
-        }
+        calculateMergeResult($checkbox);
 
         $colCheckboxes = resultCheckboxesByColIdxAndSummaryRowIdx[colIdx][summaryRowIdx];
         $rowCheckboxes = resultCheckboxesByRowIdx[rowIdx];
