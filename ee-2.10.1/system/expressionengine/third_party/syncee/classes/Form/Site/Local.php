9ee7dcdde5f288e8a6b46c4e61a9d4deb212abb6
danmooney <dan.r.mooney@gmail.com>
2015-08-22 20:45:57 -0400

    Change wording on master override/IP whitelist form field labels

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Form/Site/Local.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Form/Site/Local.php
@@ -22,17 +22,17 @@ class Syncee_Form_Site_Local extends Syncee_Form_Abstract
 {
     protected $_fields = array(
         'requests_from_remote_sites_enabled' => array(
-            'label' => 'MASTER OVERRIDE:',
-            'instructions' => 'Are remote sites allowed to call this local site?',
+            'label' => 'MASTER OVERRIDE',
+            'instructions' => 'In the OFF position, ABSOLUTELY NO remote sites are allowed to call this local site; the response will be 403 Forbidden.  Set to "ON" to allow some or all remote sites to call this local site (depending on the settings below).',
             'type' => 'dropdown',
             'options' => array(
-                0 => 'No',
-                1 => 'Yes'
+                0 => 'Off',
+                1 => 'On'
             ),
         ),
         'ip_whitelist' => array(
             'label' => 'IP Whitelist', // TODO - Add multiple inputs for this field (and lookup how to validate/convert CIDR ranges)
-            'instructions' => 'Instructions: Enter one IP per line.  CIDR notation will not be converted to IP ranges.<br><br>If left empty with master override set to \'Yes\', any remote site can make requests to this local site and view its encrypted responses.',
+            'instructions' => 'Instructions: Enter one IP per line.  CIDR notation will not be converted to IP ranges.<br><br>If left empty by default with master override set to "ON", any remote site can make requests to this local site and view its encrypted responses.',
             'type'  => 'textarea',
         ),
     );
