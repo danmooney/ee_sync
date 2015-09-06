c8177fcb453d714b9bb7dad500a8b312bad8f331
danmooney <dan.r.mooney@gmail.com>
2015-09-06 13:01:29 -0400

    Add width resizing logic after sticking rows

--- ee-2.10.1/themes/third_party/syncee/js/table-resizable.js
+++ ee-2.10.1/themes/third_party/syncee/js/table-resizable.js
@@ -3,8 +3,10 @@ $(function ($) {
         $start,
         startX,
         startWidth,
-        $tableHeaders = $('[data-resizable-table] > thead > tr > th'),
-        $tableCells = $('[data-resizable-table] > tbody > tr > td'),
+        resizableTableSelectorStr = '[data-resizable-table]',
+        $resizableTables = $(resizableTableSelectorStr),
+        $tableHeaders = $(resizableTableSelectorStr + ' > thead > tr > th'),
+        $tableCells = $(resizableTableSelectorStr + ' > tbody > tr > td'),
         startWidthAsPercentage
     ;
 
@@ -32,7 +34,7 @@ $(function ($) {
             var $th = $(e.currentTarget),
                 $thChild = $th.children(),
                 thIdx = $th.siblings('th').andSelf().index($th),
-                $correspondingTdsInColumn = $th.closest('table').find('tbody tr td:nth-child(' + (thIdx + 1) + ')'),
+                $correspondingTdsInColumn = $th.closest(resizableTableSelectorStr).find('tbody tr td:nth-child(' + (thIdx + 1) + ')'),
                 maxWidthOfCellInColumn
             ;
 
@@ -70,11 +72,109 @@ $(function ($) {
     ;
 
     $tableHeaders.each(function () {  // set pixel widths based on percentage assigned
-        $(this).width($(this).width());
+        var width = $(this).width();
+
+        $(this).width(width).data('stored-width', width);
     });
 
-    $tableCells.each(function () {  // set pixel widths based on percentage assigned
-        $(this).width($(this).width());
+    $tableCells.each(function () {  // set pixel widths based on width assigned to corresponding header column
+        var $tableCell = $(this),
+            $resizableTable = $tableCell.closest(resizableTableSelectorStr),
+            idx = $tableCells.index($tableCell),
+            colIdx = $tableCell.data('col-idx'),
+            width = $tableCell.width()
+        ;
+
+        $tableCell.width(width).data('stored-width', width);
+
+        if (typeof colIdx !== 'undefined') {
+            $resizableTable.find('[data-col-idx="' + colIdx + '"]').width(width);
+        }
+    });
+
+    function evaluateMaxWidthsInEachColumn () {
+
+    }
+
+    $(document).on('stuck', resizableTableSelectorStr + ' tr', function (e) {
+        var $table = $(e.currentTarget).closest('table'),
+            $tableRows = $table.find('tr'),
+            $tableCells = $tableRows.find('td, th'),
+            colCellsByIdx = [],
+            colIdxsResizedMaxWidths = [],
+            highestColIdx = $table.find('tr td').last().data('col-idx'),
+            i
+        ;
+
+        for (i = 0; i <= 0/*<= highestColIdx*/; i += 1) {
+            colIdxsResizedMaxWidths[i] = 0;
+            colCellsByIdx[i]           = $tableCells.filter('[data-col-idx="' + i + '"]');
+
+            colCellsByIdx[i].slice(0, 10).each(function () {
+                var $tableCell = $(this),
+                    width
+                ;
+
+                if ($tableCell.parent('.stuck').length) {
+                    return true; // continue
+                }
+
+                width = $tableCell.width();
+
+                if (width > colIdxsResizedMaxWidths[i]) {
+                    colIdxsResizedMaxWidths[i] = width;
+                }
+            });
+        }
+
+        for (i = 0; i <= 0; i += 1) {
+            colCellsByIdx[i] = colCellsByIdx[i].filter(function () {
+                return !$(this).closest('.nested-table-container').length
+            });
+
+            colCellsByIdx[i].each(function (idx) {
+                var $tableCell = $(this),
+                    storedWidthData = $tableCell.data('stored-width'),
+                    tableCellWidth = $tableCell.width(),
+                    maxColumnWidth = colIdxsResizedMaxWidths[i],
+                    newTableCellWidth = storedWidthData
+                ;
+
+                if (typeof storedWidthData === 'undefined' || tableCellWidth >= maxColumnWidth) {
+                    return true; // continue
+                }
+
+                if (tableCellWidth !== storedWidthData) {
+                    //newTableCellWidth += (storedWidthData - tableCellWidth);
+                }
+
+                //if (tableCellWidth !== colIdxsResizedMaxWidths[i]) {
+                    newTableCellWidth += (colIdxsResizedMaxWidths[i] - tableCellWidth) /*+ 1*/;
+                //}
+
+                if (tableCellWidth !== newTableCellWidth) {
+                    $tableCell.width(newTableCellWidth);
+
+                    if (!$tableCell.parent('.stuck').length) { // if has already altered stuck and non stuck cell in column, then break out of loop
+                        colCellsByIdx[i].slice(idx).width(newTableCellWidth);
+                        return false;
+                    }
+                }
+            });
+        }
+    });
+
+
+    // reset all cells back to original stored width when everything's unstuck
+    $(document).on('all-unstuck', resizableTableSelectorStr, function setAllCellsToOriginalStoredWidth () {
+        $(this).find('td, th').each(function () {
+            var $tableCell = $(this),
+                storedWidthData = $tableCell.data('stored-width');
+
+            if (typeof storedWidthData !== 'undefined') {
+                $(this).width(storedWidthData);
+            }
+        });
     });
 
 
