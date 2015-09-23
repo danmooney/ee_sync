12f3f95492cadf4209392c960cdda0e6905eb212
danmooney <dan.r.mooney@gmail.com>
2015-09-23 19:13:36 -0400

    Abstract out synchronization profile creation from factory

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Synchronization/Profile/Factory.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Site/Synchronization/Profile/Factory.php
@@ -26,9 +26,22 @@ class Syncee_Site_Synchronization_Profile_Factory
      */
     private $_site_group;
 
-    public function __construct(Syncee_Site_Group $site_group)
+    /**
+     * @var Syncee_Collection_Library_Comparator_Abstract
+     */
+    private $_comparator_library;
+
+    /**
+     * @var Syncee_Request_Remote_Entity_Abstract
+     */
+    private $_remote_entity;
+
+
+    public function __construct(Syncee_Site_Group $site_group, Syncee_Collection_Library_Comparator_Abstract $comparator_library, Syncee_Request_Remote_Entity_Abstract $remote_entity)
     {
-        $this->_site_group   = $site_group;
+        $this->_site_group         = $site_group;
+        $this->_comparator_library = $comparator_library;
+        $this->_remote_entity      = $remote_entity;
     }
 
     /**
@@ -40,7 +53,7 @@ class Syncee_Site_Synchronization_Profile_Factory
         $site_group                    = $this->_site_group;
         $site_collection               = $site_group->getSiteCollection();
 
-        $channel_comparison_library    = $site_collection->getChannelComparisonCollectionLibrary();
+        $channel_comparison_library    = $site_collection->getComparisonCollectionLibrary($this->_comparator_library, $this->_remote_entity);
 
         $synchronization_profile       = new Syncee_Site_Synchronization_Profile();
 
