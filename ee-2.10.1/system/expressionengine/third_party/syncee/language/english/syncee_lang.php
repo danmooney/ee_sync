33ef6c4c9dcf512153ef0be844bbbd8de89e2e8b
danmooney <dan.r.mooney@gmail.com>
2015-09-26 12:35:22 -0400

    Add BAD_PUBLIC_KEY language definition

--- ee-2.10.1/system/expressionengine/third_party/syncee/language/english/syncee_lang.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/language/english/syncee_lang.php
@@ -1,8 +1,9 @@
 <?php
 
-require dirname(__FILE__) . '/../../_init.php';
+require_once dirname(__FILE__) . '/../../_init.php';
 
 $lang = array(
     'syncee_module_name'        => 'SyncEE',
-    'syncee_module_description' => 'Synchronizes and merges channel data and fields from another ExpressionEngine install'
+    'syncee_module_description' => 'Synchronizes and merges channel data and fields from another ExpressionEngine install',
+    'BAD_PUBLIC_KEY'            => 'Bad public key.',
 );
