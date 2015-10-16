0a16d1665eb57e3617985dde6fe8ddcd52486ff6
danmooney <dan.r.mooney@gmail.com>
2015-10-15 21:58:22 -0400

    Add merge summary results table and add scrollTo functionality to comparison summary rows in comparison table

--- ee-2.10.1/themes/third_party/syncee/js/comparison-summary-update.js
+++ ee-2.10.1/themes/third_party/syncee/js/comparison-summary-update.js
@@ -1,7 +1,81 @@
 Syncee.updateSummaryBasedOnRow = function ($row, isMerged) {
-    var $questionMark = $row.find('.question-mark');
+    var $questionMark = $row.find('.question-mark'),
+        uniqueIdentifierValue = $row.data('name'),
+        $mergeResultSummaryTargetSite = $row.find('.merge-result-summary-target'),
+        $mergeResultSummarySourceSites = $row.find('.merge-result-summary-source-site'),
+        mergeResultSummarySourceSiteNames = [],
+        mergeResultSummarySourceSiteNamesHtmlCollection = [],
+        mergeIsAllFromTargetSite = isMerged && !$mergeResultSummarySourceSites.length,
+        $summarizationTableCell = $('#syncee').find('.summary-table [data-name="' + uniqueIdentifierValue + '"]').siblings('.summary'),
+        $summarizationRow = $summarizationTableCell.closest('tr'),
+        questionMarkHtml = ''
+    ;
+
+    $mergeResultSummarySourceSites.each(function () {
+        var siteName = $(this).text().replace(/:\s+\d+\/\d+$/, '');
+
+        mergeResultSummarySourceSiteNames.push(siteName);
+    });
+
+    [].forEach.call(mergeResultSummarySourceSiteNames, function (siteName) {
+        mergeResultSummarySourceSiteNamesHtmlCollection.push(''); // TODO
+    });
 
     if (isMerged) {
-        $questionMark.changeQtipContent('I am merged!');
+        if (mergeIsAllFromTargetSite) {
+            questionMarkHtml += 'requires no further input.  However, nothing will change since all options have been chosen from the local site.';
+        } else {
+            questionMarkHtml += 'requires no further input.  It will be merged from ' + mergeResultSummarySourceSiteNames.join(' ');
+        }
+    } else {
+        questionMarkHtml += 'still requires your input.  No action will be taken until all inputs are filled.';
+    }
+
+    $summarizationTableCell.html(questionMarkHtml);
+    questionMarkHtml = '<strong><i>' + uniqueIdentifierValue + '</i></strong> ' + questionMarkHtml;
+    $questionMark.changeQtipContent(questionMarkHtml);
+
+    if (isMerged) {
+        $summarizationRow.removeClass('negative').addClass('positive');
+    } else {
+        $summarizationRow.removeClass('positive').addClass('negative');
     }
 };
+
+// bind summary name event click to bring summary row to top of viewport
+$(function ($) {
+    $('#syncee').find('.summary-table [data-name] a').on('click', function scrollToSummaryRow (e) {
+        var uniqueIdentifierValue = $(this).closest('[data-name]').data('name'),
+            $summaryRowToScrollTo = $('.comparison-summary[data-name="' + uniqueIdentifierValue + '"]'),
+            $summaryRowIsStuck = $summaryRowToScrollTo.hasClass('stuck'),
+            combinedStuckRowHeight = 0
+        ;
+
+        e.preventDefault();
+
+        if ($summaryRowToScrollTo.length > 1) {
+            $summaryRowToScrollTo = $summaryRowToScrollTo.filter('.sticky-placeholder');
+        }
+
+        $('.stuck')
+            .filter(function () {
+                var isInViewport = $(this).get(0).getBoundingClientRect().top >= 0,
+                    isNotComparisonSummary = !$(this).hasClass('comparison-summary')
+                ;
+
+                if ($summaryRowIsStuck) {
+                    return isInViewport && isNotComparisonSummary;
+                } else {
+                    return isInViewport;
+                }
+
+            }).each(function () {
+                combinedStuckRowHeight += $(this).outerHeight();
+            })
+        ;
+
+        $('html, body').animate({
+            scrollTop: $summaryRowToScrollTo.offset().top - combinedStuckRowHeight
+        }, 1000);
+    });
+});
