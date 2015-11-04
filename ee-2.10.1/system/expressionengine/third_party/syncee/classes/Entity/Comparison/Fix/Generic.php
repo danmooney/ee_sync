ab2b83a21f6853fd1fed80f046240db3455e200d
danmooney <dan.r.mooney@gmail.com>
2015-11-04 05:59:57 -0500

    Add Syncee_Entity_Comparison_Fix_* classes

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Comparison/Fix/Generic.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Entity_Comparison_Fix_Generic extends Syncee_Entity_Comparison_Fix_Abstract
+{
+    /**
+     * @var Syncee_Entity_Comparison
+     */
+    protected $_comparison;
+
+    public function __construct(Syncee_Entity_Comparison $comparison)
+    {
+        $this->_comparison = $comparison;
+    }
+
+    public function modifyComparateValueAndOrPerformMiscTasksByDecisionPayload(&$comparate_value, array $decision_payload)
+    {
+        // nothing to do in generic fixes; leave comparate value as is and perform no tasks
+        return;
+    }
+}
