da76a2671fed24cd2b3b3c74f1d99ac78b1f29f8
danmooney <dan.r.mooney@gmail.com>
2015-11-07 17:52:19 -0500

    Implement Syncee_Entity_Comparison_Fix_ChannelField_Column_FieldFormats::performMiscTasksByDecisionPayloadAndActiveRecordRowAfterSave

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Comparison/Fix/ChannelField/Column/FieldFormats.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Comparison/Fix/ChannelField/Column/FieldFormats.php
@@ -20,5 +20,34 @@ if (!defined('SYNCEE_PATH')) {
 
 class Syncee_Entity_Comparison_Fix_ChannelField_Column_FieldFormats extends Syncee_Entity_Comparison_Fix_Generic
 {
+    /**
+     * Insert into exp_field_formatting
+     * @param $comparate_value
+     * @param array $decision_payload
+     * @param Syncee_ActiveRecord_Abstract $active_record_row
+     * @throws Syncee_Exception
+     * @return self
+     */
+    public function performMiscTasksByDecisionPayloadAndActiveRecordRowAfterSave($comparate_value, array $decision_payload, Syncee_ActiveRecord_Abstract $active_record_row)
+    {
+        if (!is_array($comparate_value)) { // field_formats SHOULD be an array, throw exception if this isn't the case
+            throw new Syncee_Exception('Comparate value passed to ' . __METHOD__ . ' should be an array, ' . gettype($comparate_value) . ' given');
+        }
 
+        $field_formats =& $comparate_value;
+
+        // first purge any rows that may exist in exp_field_formatting for active record row's primary key value
+        ee()->db->delete('field_formatting', array(
+            'field_id' => $active_record_row->getPrimaryKeyValues(true)
+        ));
+
+        foreach ($field_formats as $field_format) {
+            ee()->db->insert('field_formatting', array(
+                'field_id'  => $active_record_row->getPrimaryKeyValues(true),
+                'field_fmt' => $field_format['field_fmt']
+            ));
+        }
+
+        return $this;
+    }
 }
