d371d7952531a57a5cf29875787e0c09bfea6ed9
danmooney <dan.r.mooney@gmail.com>
2015-11-13 21:55:22 -0500

    Add EE3 Support

---
+++ ee-2.10.1/system/expressionengine/third_party/field_group_clarifier/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+      'author'         => 'Dan Mooney',
+      'author_url'     => 'http://sync.ee',
+      'name'           => 'Field Group Clarifier',
+      'description'    => 'Shows the field group you are working on while adding or updating channel fields.',
+      'version'        => '1.0',
+      'namespace'      => 'DanMooney\FieldGroupClarifier',
+      'settings_exist' => false
+);
--- ee-2.10.1/system/expressionengine/third_party/field_group_clarifier/ext.field_group_clarifier.php
+++ ee-2.10.1/system/expressionengine/third_party/field_group_clarifier/ext.field_group_clarifier.php
@@ -2,6 +2,8 @@
 
 class Field_Group_Clarifier_Ext
 {
+    private static $_channel_field_group_name;
+
     public $name             = 'Field Group Clarifier';
 	public $version          = '1.0';
 	public $description      = 'Shows the field group you are working on while adding or updating channel fields.';
@@ -39,49 +41,70 @@ class Field_Group_Clarifier_Ext
 
     public function getAndStoreFieldGroupName($field, $method, $parameters)
     {
-	    static $channel_field_group_name;
-
 	    $should_return = (
-		    isset($channel_field_group_name) ||
+		    isset(self::$_channel_field_group_name) ||
 		    $method !== 'display_settings' ||
 		    (isset($_POST) && $_POST) ||
 		    REQ !== 'CP' ||
-		    (!isset($_GET['C']) || $_GET['C'] !== 'admin_content') ||
-		    (!isset($_GET['M']) || $_GET['M'] !== 'field_edit')
+		    (!isset($_GET['C']) || !in_array($_GET['C'], array('admin_content', 'channels'))) ||
+		    (!isset($_GET['M']) || !in_array($_GET['M'], array('field_edit', 'fields')))
 	    );
 
 	    if ($should_return) {
 		    return $parameters;
 	    }
 
-		$group_id = isset($_GET['group_id']) ? $_GET['group_id'] : null;
+        if (isset($_GET['group_id'])) {
+		    $group_id = $_GET['group_id'];
+        } elseif (isset($field) && is_object($field) && isset($field->settings) && is_array($field->settings) && isset($field->settings['group_id'])) {
+            $group_id = $field->settings['group_id'];
+        } elseif (isset($_SERVER['REQUEST_URI']) && preg_match('#/fields/create/(\d+)#', $_SERVER['REQUEST_URI'], $matches)) {
+            $group_id = $matches[1];
+        }
 
-	    if (!$group_id) {
+	    if (!isset($group_id)) {
 		    return $parameters;
 	    }
 
-	    $channel_field_group_name = ee()->db->select('group_name')->from('field_groups')->where('group_id', $group_id)->get()->row('group_name');
+	    $channel_field_group_name = self::$_channel_field_group_name = ee()->db->select('group_name')->from('field_groups')->where('group_id', $group_id)->get()->row('group_name');
 
 	    $ee = ee();
 
-	    if (isset($ee->cp) && method_exists($ee->cp, 'set_variable') && function_exists('lang')) {
-            $is_creating_new_custom_field = !isset($_GET['field_id']);
+        if (isset($_GET['field_id'])) {
+            $field_id = $_GET['field_id'];
+        } elseif (isset($field) && is_object($field) && isset($field->settings) && is_array($field->settings) && isset($field->settings['field_id'])) {
+            $field_id = $field->settings['field_id'];
+        }
 
-		    $lang_key = $is_creating_new_custom_field ? 'create_new_custom_field' : 'edit_field';
+        $is_creating_new_custom_field = !isset($field_id);
 
-            if (!lang($lang_key)) {
-                if ($is_creating_new_custom_field) {
-                    $action = 'Create a New Channel Field';
-                } else {
-                    $action = 'Edit Field';
-                }
+        $lang_key = $is_creating_new_custom_field ? 'create_new_custom_field' : 'edit_field';
+
+        if (!lang($lang_key)) {
+            if ($is_creating_new_custom_field) {
+                $action = 'Create a New Channel Field';
             } else {
-                $action = lang($lang_key);
+                $action = 'Edit Field';
             }
+        } else {
+            $action = lang($lang_key);
+        }
 
+	    if (isset($ee->cp) && method_exists($ee->cp, 'set_variable') && function_exists('lang')) {
 			$ee->cp->set_variable('cp_page_title', sprintf('%s for Field Group "%s"', $action, $channel_field_group_name));
 	    } elseif (isset($ee->view) && is_object($ee->view)) {
-	        $ee->view->cp_page_title .= sprintf(' for Field Group "%s"', $channel_field_group_name);
+            $cp_page_title_not_set_yet_and_will_most_likely_be_overwritten_later = !isset($ee->view->cp_page_title) || empty($ee->view->cp_page_title);
+
+            if ($cp_page_title_not_set_yet_and_will_most_likely_be_overwritten_later) {
+                $lang = ee()->lang;
+                $lang->language[$lang_key] = sprintf('%s for Field Group "%s"', $action, $channel_field_group_name);
+
+                if ($lang_key === 'create_new_custom_field') {
+                    $lang->language['create_field'] = $lang->language[$lang_key];
+                }
+            } else {
+	            $ee->view->cp_page_title .= sprintf(' for Field Group "%s"', $channel_field_group_name);
+            }
 	    }
 
 	    return $parameters;
