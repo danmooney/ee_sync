0bf2d01e5425b1b28f7e1959d45cb24ba7d1d67f
danmooney <dan.r.mooney@gmail.com>
2015-12-29 21:15:30 -0500

    Add serialize/base64 modifier on generic fix columns if they are supposed to be serialized and base64-encoded

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Comparison/Fix/Generic.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Comparison/Fix/Generic.php
@@ -32,7 +32,16 @@ class Syncee_Entity_Comparison_Fix_Generic extends Syncee_Entity_Comparison_Fix_
 
     public function modifyComparateValueByDecisionPayloadBeforeSave(&$comparate_value, array $decision_payload)
     {
-        // nothing to do in generic fixes; leave comparate value as is and perform no tasks
+        $comparate_value_is_json_and_needs_to_be_serialized_and_base64_encoded = (
+            $this->_comparison->comparateColumnIsSerializedAndBase64Encoded() &&
+            ($comparate_value_json_decoded = json_decode($comparate_value, true)) &&
+            is_array($comparate_value_json_decoded)
+        );
+
+        if ($comparate_value_is_json_and_needs_to_be_serialized_and_base64_encoded) {
+            $comparate_value = base64_encode(serialize($comparate_value_json_decoded));
+        }
+
         return $this;
     }
 
