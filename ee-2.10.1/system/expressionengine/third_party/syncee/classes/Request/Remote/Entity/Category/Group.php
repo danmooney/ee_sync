db0936c9d9e1f25e2e1d47dd734c3ab2ea8df766
danmooney <dan.r.mooney@gmail.com>
2016-01-03 16:01:18 -0500

    Add Syncee_Request_Remote_Entity_Category_Group

---
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Request/Remote/Entity/Category/Group.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('SYNCEE_PATH')) {
+    $current_dir = dirname(__FILE__);
+    $i           = 1;
+
+    while (($ancestor_realpath = realpath($current_dir . str_repeat('/..', $i++)) . '/_init.php') && !is_readable($ancestor_realpath)) {
+        $is_at_root = substr($ancestor_realpath, -1) === PATH_SEPARATOR;
+        if ($is_at_root) {
+            break;
+        }
+    }
+
+    if (!is_readable($ancestor_realpath)) {
+        show_error('Could not find _init.php for module');
+    }
+
+    require_once $ancestor_realpath;
+}
+
+class Syncee_Request_Remote_Entity_Category_Group extends Syncee_Request_Remote_Entity_Abstract
+{
+    protected $_collection_class_name = 'Syncee_Collection_Generic';
+
+    public function getName()
+    {
+        return 'category group';
+    }
+
+    public function queryDatabaseAndGenerateCollection()
+    {
+        $rows = ee()->db->select('*')->from('category_groups')->get()->result_array();
+
+        $class_name = $this->getCollectionClassName();
+        return new $class_name($rows);
+    }
+}
