e2b13f714d709720cb74f36fe54209dd40f4da0e
danmooney <dan.r.mooney@gmail.com>
2016-02-21 16:51:45 -0500

    Add Syncee_Entity_Reference_Collection::appendToCollectionAsArray/_getCollectionSubsetRowModelBasedOnKey

--- ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Reference/Collection.php
+++ ee-2.10.1/system/expressionengine/third_party/syncee/classes/Entity/Reference/Collection.php
@@ -20,5 +20,41 @@ if (!defined('SYNCEE_PATH')) {
 
 class Syncee_Entity_Reference_Collection extends Syncee_Entity_Comparate_Collection_Abstract
 {
-    protected $_row_model = 'Syncee_Entity_Generic';
+    protected $_row_model = 'Syncee_Entity_Reference_Collection_Subset';
+
+    public function appendToCollectionAsArray(array $row, $key = null)
+    {
+        $collection_subset_row_model = $this->_getCollectionSubsetRowModelBasedOnKey($key);
+
+        $row = array_map(function ($arr) use ($collection_subset_row_model) {
+            return new $collection_subset_row_model($arr);
+        }, $row);
+
+        parent::appendToCollectionAsArray($row, $key);
+        $this->_rows[$key]->setRowModel($collection_subset_row_model);
+    }
+
+    private function _getCollectionSubsetRowModelBasedOnKey($key)
+    {
+        $remote_entity_objects    = Syncee_Request_Remote_Entity_Abstract::getAllRemoteEntityClassObjects();
+
+        /**
+         * @var $remote_entity_object Syncee_Request_Remote_Entity_Abstract
+         * @var $collection_row Syncee_Entity_Reference_Collection_Subset
+         */
+        foreach ($remote_entity_objects as $remote_entity_object) {
+            if ($remote_entity_object->getName() === $key) {
+                $collection_subset_row_model = $remote_entity_object->getEmptyCollectionInstance()->getRowModel();
+                break;
+            }
+        }
+
+        if (!isset($collection_subset_row_model)) {
+            $collection_row_model        = $this->_row_model;
+            $collection_row              = new $collection_row_model();
+            $collection_subset_row_model = $collection_row->getRowModel();
+        }
+
+        return $collection_subset_row_model;
+    }
 }
